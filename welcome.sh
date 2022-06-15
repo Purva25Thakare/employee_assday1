be441cddaf5aa25ad3f0f7d19e6cfacceccbd675
Purva25Thakare <thakarepurva25@gmail.com>
2022-06-15 19:39:39 +0530

    Added:Employee Attendance

--- welcome.sh
+++ welcome.sh
@@ -1,1 +1,9 @@
 echo "Welcome to Employee World"
+att=$(( RANDOM%2 ))
+echo "Attendace of Employee is: " $att
+	if (( $att==0 ))
+then
+	echo "Employee is Absent"
+else
+	echo "Employee is Present"
+fi
